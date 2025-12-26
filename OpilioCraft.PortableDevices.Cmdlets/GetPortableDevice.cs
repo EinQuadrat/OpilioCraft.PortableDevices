a5efdbc5385258e14ec6eb5fc22e4fd63e79711c
EinQuadrat <carsten.schaefer@mailbox.org>
2025-12-26 11:31:25 +0100

    Onoing transition to WinRT.

---
+++ OpilioCraft.PortableDevices.Cmdlets/GetPortableDevice.cs
@@ -0,0 +1,50 @@
+﻿using System.Management.Automation;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Portable;
+
+namespace OpilioCraft.PortableDevices.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "PortableDevice")]
+    [OutputType(typeof(DeviceInformation))]
+    public class GetPortableDevice : PSCmdlet {
+        // params
+        [Parameter(ParameterSetName = "ById")]
+        [Parameter(Mandatory = false, HelpMessage = "If specified, retrieves only devices that match the given device id.")]
+        public string ById { get; set; } = string.Empty;
+
+        [Parameter(ParameterSetName = "ByName")]
+        [Parameter(Mandatory = false, HelpMessage = "If specified, retrieves only devices that match the given name.")]
+        public string ByName { get; set; } = string.Empty;
+
+        // behaviour
+        protected override async void EndProcessing()
+        {
+            base.EndProcessing();
+
+            try
+            {
+                string selector = StorageDevice.GetDeviceSelector(); // AQS-String für tragbare Speichergeräte (WPD/MTP, USB-Massenspeicher)
+                IEnumerable<DeviceInformation> devices = await DeviceInformation.FindAllAsync(selector); // Alle passenden Geräte holen
+
+                if (!string.IsNullOrEmpty(ById))
+                {
+                    devices = devices.Where(dev => dev.Id.Equals(ById));
+                }
+
+                if (!string.IsNullOrEmpty(ByName))
+                {
+                    devices = devices.Where(dev => dev.Name.Equals(ByName));
+                }
+
+                foreach (var dev in devices)
+                {
+                    WriteObject(sendToPipeline: dev);
+                }
+            }
+            catch (Exception exn)
+            {
+                WriteError(errorRecord: new ErrorRecord(exn, null, ErrorCategory.DeviceError, this));
+            }
+        }
+    }
+}
--- OpilioCraft.PortableDevices.Cmdlets/GetPortableDeviceItems.cs
+++ OpilioCraft.PortableDevices.Cmdlets/GetPortableDeviceItems.cs
@@ -1,51 +1,52 @@
-﻿using System.Management.Automation;
-using Windows.Devices.Portable;
+﻿//using System.Management.Automation;
+//using Windows.Devices.Enumeration;
+//using Windows.Devices.Portable;
 
-namespace OpilioCraft.PortableDevices.Cmdlets
-{
-    [Cmdlet(VerbsCommon.Get, "PortableDeviceItems")]
-    [OutputType(typeof(StorageDevice))]
-    public class GetPortableDeviceItems : PSCmdlet {
-        // params
-        [Parameter(Position = 0, Mandatory = true)]
-        public string Device { get; set; } = string.Empty;
+//namespace OpilioCraft.PortableDevices.Cmdlets
+//{
+//    [Cmdlet(VerbsCommon.Get, "PortableDeviceItem")]
+//    [OutputType(typeof(StorageDevice))]
+//    public class GetPortableDeviceItems : PSCmdlet {
+//        // params
+//        [Parameter(Position = 0, Mandatory = true)]
+//        public DeviceInformation Device { get; set; }
 
-        [Parameter()]
-        public string Path { get; set; } = string.Empty;
+//        [Parameter()]
+//        public string Path { get; set; } = string.Empty;
 
-        [Parameter()]
-        public SwitchParameter Refresh { get; set; } = new(false);
+//        [Parameter()]
+//        public SwitchParameter Refresh { get; set; } = new(false);
 
-        // cmdlet functionality
-        protected override void ProcessRecord()
-        {
-            try
-            {
-                // lookup device
-                var device = PortableDeviceManager.GetDeviceByName(Device, refresh: Refresh.ToBool());
+//        // cmdlet functionality
+//        protected override void ProcessRecord()
+//        {
+//            try
+//            {
+//                // lookup device
+//                //var device = PortableDeviceManager.GetDeviceByName(Device, refresh: Refresh.ToBool());
 
-                if (device is not null)
-                {
-                    var rootFolder = StorageDevice.FromId(device.Id);
+//                //if (device is not null)
+//                //{
+//                //    var rootFolder = StorageDevice.FromId(device.Id);
 
-                    // navigate to folder
-                    var folder = rootFolder.GetFolder(Path);
+//                //     navigate to folder
+//                //    var folder = rootFolder.GetFolder(Path);
 
-                    // enumerate content
-                    foreach (var item in folder.ChildItems)
-                    {
-                        WriteObject(sendToPipeline: item);
-                    }
-                }
-                else
-                {
-                    throw new Exception($"Device '{Device}' not found.");
-                }
-            }
-            catch (Exception exn)
-            {
-                WriteError(errorRecord: new ErrorRecord(exn, null, ErrorCategory.DeviceError, this));
-            }
-        }
-    }
-}
+//                //     enumerate content
+//                //    foreach (var item in folder.ChildItems)
+//                //    {
+//                //        WriteObject(sendToPipeline: item);
+//                //    }
+//                //}
+//                //else
+//                //{
+//                //    throw new Exception($"Device '{Device}' not found.");
+//                //}
+//            }
+//            catch (Exception exn)
+//            {
+//                WriteError(errorRecord: new ErrorRecord(exn, null, ErrorCategory.DeviceError, this));
+//            }
+//        }
+//    }
+//}
